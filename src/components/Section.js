aa3f9b5d6f1c2647f1b0fcf5e0f078673d012457
AJ Auntor <auntorknocks@gmail.com>
2021-08-29 00:29:01 +0600

    Section Styled component almost design

--- src/components/Section.js
+++ src/components/Section.js
@@ -9,14 +9,17 @@ const Section = () => {
                 <h1>Model S</h1>
                 <p>Order Online for Touchless Delivery</p>
             </ItemText>
-            <ButtonGroup>
-                <LeftButton>
-                        Custom Order
-                </LeftButton>
-                <RightButton>
-                        Exisiting Inventory
-                </RightButton>
-            </ButtonGroup>
+            <Buttons>
+                <ButtonGroup>
+                    <LeftButton>
+                            Custom Order
+                    </LeftButton>
+                    <RightButton>
+                            Exisiting Inventory
+                    </RightButton>
+                </ButtonGroup>
+                <DownArrow src="/images/down-arrow.svg" />
+            </Buttons>
         </Wrap>
     );
 };
@@ -30,6 +33,10 @@ const Wrap = styled.div`
     background-repeat: no-repeat;
     background-size: cover;
     background-image: url('images/model-s.jpg');
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
 `
 
 const ItemText = styled.div `
@@ -37,11 +44,30 @@ const ItemText = styled.div `
     text-align: center;
 `
 const ButtonGroup = styled.div`
-
+    display: flex;
+    margin-bottom: 20px;
 `
 const LeftButton = styled.div`
-
+background-color: rgba(23, 26, 32, 0.8);
+height: 40px;
+width: 256px;
+color: white;
+display: flex;
+justify-content: center;
+align-items: center;
+border-radius: 100px;
+opacity: 0.58;
+text-transform: uppercase;
+font-size: 12px;
+cursor: pointer;
+margin-left: 10px;
 `
-const RightButton = styled.div`
+const RightButton = styled(LeftButton)``
 
+const DownArrow = styled.img`
+margin-top: 20px;
+height: 40px;
+width: 100%;
+animation: animateDown infinite 1.5s;
 `
+const Buttons = styled.div``
