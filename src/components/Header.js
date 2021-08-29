abf1a77c20a5cbff0ac63bcc41e63eb86515137d
AJ Auntor <auntorknocks@gmail.com>
2021-08-29 11:52:08 +0600

    Matirial Ul core

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Header.css'
 import styled from 'styled-components';
+import MenuIcon from '@material-ui/icons/Menu';
 
 const Header = () => {
     return (
@@ -8,6 +9,17 @@ const Header = () => {
             <a>
                 <img src="/images/logo.svg" alt="" />
             </a>
+            <Menu>
+                <a href="#"> Model S </a>
+                <a href="#"> Model 3 </a>
+                <a href="#"> Model X </a>
+                <a href="#"> Model Y </a>
+            </Menu>
+            <RightMenu>
+                    <a href="#"> Shop </a>
+                    <a href="#"> Tesla Account</a>
+            </RightMenu>
+            <CustomMenu />
         </Container>
     );
 };
@@ -15,5 +27,36 @@ const Header = () => {
 export default Header;
 
 const Container = styled.div`
+    min-height: 60px;
+    position: fixed;
+    display: flex;
+    align-items: center;
+    padding: 0 20px;
+    top: 0;
+    left: 0;
+    right: 0;
+`
+const Menu = styled.div`
+    display:flex;
+    align-items: center;
+    justify-content: center;
+    flex: 1;
 
+    a {
+        font-weight: 600;
+        text-transform: uppercase;
+        padding: 0 10px;
+        flex-wrap: nowrap;
+    }
+`
+const RightMenu = styled.div`
+    a {
+        font-weight: 600;
+        text-transform: uppercase;
+        margin-right: 10px;
+        flex-wrap: nowrap;
+    }
+`
+const CustomMenu = styled(MenuIcon)`
+    
 `
